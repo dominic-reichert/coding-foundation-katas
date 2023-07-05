9ff4f9389f9e4ff6e0185b0e5e10cc3e7899ef5e
dominic-reichert <dominicreichert92@gmail.com>
2023-07-05 11:10:29 +0200

    adds solution to sum-app.js

--- sum-app/sum-app.js
+++ sum-app/sum-app.js
@@ -1,3 +1,21 @@
 class SumApp {
-  // Add you solution here
+  numbers = [];
+
+  addNumber(n) {
+    this.numbers.push(n);
+  }
+
+  getSum() {
+    let result = 0;
+    if (this.numbers.length === 0) {
+      return result;
+    } else {
+      this.numbers.forEach((element) => (result += element));
+    }
+    return result;
+  }
+
+  reset() {
+    this.numbers = [];
+  }
 }
