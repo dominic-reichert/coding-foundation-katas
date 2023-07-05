f0b205dc6386563df5f626f9bc7bf4b382e7750e
dominic-reichert <dominicreichert92@gmail.com>
2023-07-05 11:29:34 +0200

    adds solution for zip-strings.js

--- zip-strings/zip-strings.js
+++ zip-strings/zip-strings.js
@@ -1,3 +1,13 @@
 function zipStrings(strA, strB) {
-  // Add you solution here
+  let result = "";
+  for (let i = 0; i < strA.length || i < strB.length; i++) {
+    if (i < strA.length) {
+      result += strA.charAt(i);
+    }
+    if (i < strB.length) {
+      result += strB.charAt(i);
+    }
+  }
+
+  return result;
 }
