33cb38363b188e3ccd276fea4bd0c85d7ae9fa0e
dominic-reichert <dominicreichert92@gmail.com>
2023-07-05 09:10:59 +0200

    adds solution to shopping-cart.js

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -1,3 +1,32 @@
 class ShoppingCart {
-  // todo: implement your code here
+  Cart = [];
+  addToCart(description, price, quantity) {
+    this.Cart.push({
+      description: description,
+      price: price,
+      quantity: quantity,
+    });
+    return "Added item to cart!";
+  }
+
+  getCartPrice() {
+    let price = 0;
+    for (let item of this.Cart) {
+      price += item.price * item.quantity;
+    }
+    return price;
+  }
+
+  removeFromCart(description, quantity) {
+    if (quantity === undefined) {
+      this.Cart = this.Cart.filter((e) => {
+        return e.description !== description;
+      });
+    } else {
+      const index = this.Cart.findIndex((object) => {
+        return object.description === description;
+      });
+      this.Cart[index].quantity -= quantity;
+    }
+  }
 }
